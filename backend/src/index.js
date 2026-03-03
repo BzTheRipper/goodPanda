e222d92a8055a5adb43c47886e3cde6e0cf4f5fd
BzTheRipper <bayzid.baytur.redwan87@gmail.com>
2026-03-03 15:01:02 +0600

    added dist for express 5 production

--- backend/src/index.js
+++ backend/src/index.js
@@ -76,9 +76,9 @@ app.use('/api/auth', authRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  })
+  app.get("(.*)", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 }
 
 server.listen(backendPort, () => {
